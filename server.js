215dc8320f93c402a8523545091ed2d87bb05571
Ricardo Pérez <ricardopg1987@gmail.com>
2025-06-17 09:50:04 -0400

    Versión completa y actualizada1 con QR funcional

--- server.js
+++ server.js
@@ -11,27 +11,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Página principal para el "panel de control"
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'control.html'));
 });
-
-// Página QR (para proyectar el QR)
 app.get('/qr', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'qr.html'));
 });
-
-// Página destino del QR (lo que escanea la gente)
 app.get('/hack', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Socket.io
 io.on('connection', (socket) => {
     console.log('New client connected');
-
     socket.on('hack', () => {
-        // Manda el evento a todos los clientes (excepto el control)
         io.emit('startHack');
     });
 });
